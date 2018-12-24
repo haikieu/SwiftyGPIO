4608af42913ce01680318c2cef8c2f2a43576317
Hai Kieu <haikieu2907@gmail.com>
2018-12-23 23:31:14 -0800

    Implement Mock classes

--- Sources/Mocks.swift
+++ Sources/Mocks.swift
@@ -62,35 +62,39 @@ open class PWMOutputMock: PWMOutput {
 
 open class SPIMock: SPIInterface {
     
-    public func sendData(_ values: [UInt8], frequencyHz: UInt) {
-        
-    }
-    
-    public func sendData(_ values: [UInt8]) {
-        
-    }
-    
-    public func sendDataAndRead(_ values: [UInt8], frequencyHz: UInt) -> [UInt8] {
-        return []
-    }
-    
-    public func sendDataAndRead(_ values: [UInt8]) -> [UInt8] {
-        return []
-    }
-    
     public var isHardware: Bool = true
     
+    var sendDataWithFrequencyClosure: ((_ values: [UInt8],_ frequencyHz: UInt)->Void)?
+    public func sendData(_ values: [UInt8], frequencyHz: UInt) {
+        sendDataWithFrequencyClosure?(values, frequencyHz)
+    }
     
+    var sendDataClosure: ((_ values: [UInt8])->Void)?
+    public func sendData(_ values: [UInt8]) {
+        sendDataClosure?(values)
+    }
+    
+    var sendDataAndReadWithFrequencyClosure: ((_ values: [UInt8],_ frequencyHz: UInt) -> [UInt8])?
+    public func sendDataAndRead(_ values: [UInt8], frequencyHz: UInt) -> [UInt8] {
+        return sendDataAndReadWithFrequencyClosure?(values, frequencyHz) ?? []
+    }
+    
+    var sendDataAndReadClosure: ((_ values: [UInt8]) -> [UInt8])?
+    public func sendDataAndRead(_ values: [UInt8]) -> [UInt8] {
+        return sendDataAndReadClosure?(values) ?? []
+    }
 }
 
 open class OneWireMock: OneWireInterface {
     
+    var getSlavesClosure: (()->[String])?
     public func getSlaves() -> [String] {
-        return []
+        return getSlavesClosure?() ?? []
     }
     
+    var readDataClosure: ((_ slaveId: String) -> [String])?
     public func readData(_ slaveId: String) -> [String] {
-        return []
+        return readDataClosure?(slaveId) ?? []
     }
 }
 
@@ -98,80 +102,99 @@ open class ADCMock: ADCInterface {
     
     public var id: Int = 0
     
+    var getSampleClosure: (()->Int)?
     public func getSample() throws -> Int {
-        return 0
+        return getSampleClosure?() ?? 0
     }
 }
 
 open class I2CMock: I2CInterface {
     
+    var isReachableClosure: ((_ address: Int) -> Bool)?
     public func isReachable(_ address: Int) -> Bool {
-        return false
+        return isReachableClosure?(address) ?? true
     }
     
+    var setPECClosure: ((_ address: Int,_ enabled: Bool)->Void)?
     public func setPEC(_ address: Int, enabled: Bool) {
-        
+        setPECClosure?(address, enabled)
     }
     
+    var readByteClosure: ((_ address: Int) -> UInt8)?
     public func readByte(_ address: Int) -> UInt8 {
-        return 0
+        return readByteClosure?(address) ?? 0
     }
     
+    var readByteCommandClosure: ((_ address: Int,_ command: UInt8) -> UInt8)?
     public func readByte(_ address: Int, command: UInt8) -> UInt8 {
-        return 0
+        return readByteCommandClosure?(address, command) ?? 0
     }
     
+    var readWordClosure: ((_ address: Int,_ command: UInt8) -> UInt16)?
     public func readWord(_ address: Int, command: UInt8) -> UInt16 {
-        return 0
+        return readWordClosure?(address, command) ?? 0
     }
     
+    var readDataClosure: ((_ address: Int,_ command: UInt8) -> [UInt8])?
     public func readData(_ address: Int, command: UInt8) -> [UInt8] {
-        return []
+        return readDataClosure?(address,command) ?? []
     }
     
+    var writeQuickClosure: ((_ address: Int)->Void)?
     public func writeQuick(_ address: Int) {
-        
+        writeQuickClosure?(address)
     }
     
+    var writeByteClosure: ((_ address: Int,_ value: UInt8)->Void)?
     public func writeByte(_ address: Int, value: UInt8) {
-        
+        writeByteClosure?(address,value)
     }
     
+    var writeByteCommandClosure: ((_ address: Int,_ command: UInt8,_ value: UInt8)->Void)?
     public func writeByte(_ address: Int, command: UInt8, value: UInt8) {
-        
+        writeByteCommandClosure?(address,command,value)
     }
     
+    var writeWordClosure: ((_ address: Int,_ command: UInt8,_ value: UInt16)->Void)?
     public func writeWord(_ address: Int, command: UInt8, value: UInt16) {
-        
+        writeWordClosure?(address, command, value)
     }
     
+    var writeDataClosure: ((_ address: Int,_ command: UInt8,_ values: [UInt8])->Void)?
     public func writeData(_ address: Int, command: UInt8, values: [UInt8]) {
-        
+        writeDataClosure?(address,command,values)
     }
 }
 
 open class UARTMock: UARTInterface {
+    
+    var configureInterfaceClosure: ((_ speed: UARTSpeed,_ bitsPerChar: CharSize,_ stopBits: StopBits,_ parity: ParityType)->Void)?
     public func configureInterface(speed: UARTSpeed, bitsPerChar: CharSize, stopBits: StopBits, parity: ParityType) {
-        
+        configureInterfaceClosure?(speed,bitsPerChar,stopBits,parity)
     }
     
+    var readStringClosure: (()->String)?
     public func readString() -> String {
-        return ""
+        return readStringClosure?() ?? ""
     }
     
+    var readLineClosure: (()->String)?
     public func readLine() -> String {
-        return ""
+        return readLineClosure?() ?? ""
     }
     
+    var readDataClosure: (()->[CChar])?
     public func readData() -> [CChar] {
-        return []
+        return readDataClosure?() ?? []
     }
     
+    var writeStringClousure: ((_ value: String)->Void)?
     public func writeString(_ value: String) {
-        
+        writeStringClousure?(value)
     }
     
+    var writeDataClosure: ((_ values: [CChar])->Void)?
     public func writeData(_ values: [CChar]) {
-        
+        writeDataClosure?(values)
     }
 }
